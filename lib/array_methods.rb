3588839b88d5841bb7ab6c3a28b085968845c80b
Sonata Arrington <sarrington1009@gmail.com>
2020-01-10 23:25:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   while counter < array.length do
     if array[counter] == value_to_find
       return counter
-      
+      counter += 1
     end
     nil
   end
